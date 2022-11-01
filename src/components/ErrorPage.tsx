1d06a7ebad823608e63a9bfeaed1afc5dc12ba93
Jakub Vala <jakub.vala@ackee.cz>
2022-11-01 16:34:53 +0100

    ✨ Make default error message and code

--- src/components/ErrorPage.tsx
+++ src/components/ErrorPage.tsx
@@ -1,17 +1,14 @@
 import React from 'react'
-import { useRouteError, Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import classes from './ErrorPage.module.scss'
 
-const useAppRouteError = useRouteError as () => { statusCode?: number, message?: string}
-
 const ErrorPage: React.FC = () => {
-    const error = useAppRouteError()
     return (
         <>
             <div className={classes.error}>
                 <h1>Oopsie!</h1>
-                <p className={classes['error--code']}>{error.statusCode ?? 404}</p>
-                <p className={classes['error--notfound']}>{error.message ?? 'Not found'}</p>
+                <p className={classes['error--code']}>404</p>
+                <p className={classes['error--notfound']}>'Not found'</p>
                 <Link to='/'>Home</Link>
             </div>
         </>
