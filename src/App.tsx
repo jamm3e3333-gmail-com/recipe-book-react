fcc895872f6878767a9c630ee592a128dcc6ba23
Jakub Vala <jakub.vala@ackee.cz>
2022-11-01 16:31:46 +0100

    ♻️ Add BrowserRouter instead for routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,26 @@
 import Login from './components/authentication/Login'
-import Navigation from './components/UI/Navigation'
+import Navigation from './components/Navigation'
 import Welcome from './components/welcome/Welcome'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import ErrorPage from './components/ErrorPage'
 import SignUp from './components/authentication/SignUp'
-
+import RecipesContainer from './components/recipes/RecipesContainer'
+import RecipeView from './components/recipes/RecipeView'
 
 function App() {
   return (
-    <>
-      <RouterProvider
-        router={createBrowserRouter([
-          {
-            path: '/',
-            element: <Navigation />,
-            errorElement: <ErrorPage />,
-            children: [
-              {
-                path: 'login',
-                element: <Login />,
-              }, {
-                path: 'signup',
-                element: <SignUp />,
-              },
-              {
-                path: '',
-                element: <Welcome />,
-              },
-            ]
-          },
-        ])}
-      />
-    </>
+      <Routes >
+        <Route path='/' element={<Navigation />}>
+          <Route index element={<Welcome />}  />
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<SignUp />} />
+          <Route path='/recipes'>
+            <Route index element={<RecipesContainer />} />
+            <Route path=':id' element={<RecipeView />} />
+          </Route>
+          <Route path='*' element={<ErrorPage />} />
+        </Route>
+      </Routes>
   )
 }
 
